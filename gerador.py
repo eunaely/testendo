b538c3aa919e0593ff4c7bd0a29ba0234c1b83a9
Cássia Naelir de Lima <116851542+eunaely@users.noreply.github.com>
2024-01-21 12:52:15 -0300

    Add files via upload
    gerador de senhas simples

---
+++ gerador.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def gerar_senha(tamanho=12):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    return senha
+
+# Exemplo de uso
+senha_gerada = gerar_senha()
+print("Senha gerada:", senha_gerada)
